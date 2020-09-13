f0f05ac26e8b1c00642ba46cbaca9a864e8aba33
hzm <934585316@qq.com>
2020-09-13 14:46:02 +0800

    init: 项目初始化

---
+++ .umirc.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '江南烟雨',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: '/head.png',
+  outputPath: 'docs-dist',
+  mode: 'site',
+  // more config: https://d.umijs.org/config
+});
